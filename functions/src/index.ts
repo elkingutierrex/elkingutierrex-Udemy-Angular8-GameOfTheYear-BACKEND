d5b82ed35b92fba1967d28cf4f82417015fed06e
elkingutierrex <Pro-egutierrez@intergrupo.com>
2019-12-13 10:31:34 -0500

    335 - Servidor express dentro de Firebase Cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as  admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
 
 const serviceAccount = require("./serviceAccountKey.json");
 
@@ -30,3 +32,16 @@ export const helloWorld = functions.https.onRequest((request, response) => {
    response.json( juegos );
 
 });
+
+const app = express();
+app.use( cors({origin: true}));
+
+app.get('/goty', async(req,res) => {
+   const gotyRef = db.collection('goty');
+   const docsSnap = await gotyRef.get();
+   const juegos = docsSnap.docs.map( doc => doc.data() );
+
+   res.json( juegos );
+});
+
+exports.api = functions.https.onRequest( app );
