33dc8c7433e3e500ff6c3a2c6fcb0304141b2fc3
elkingutierrex <Pro-egutierrez@intergrupo.com>
2019-12-13 10:10:15 -0500

    334 - Could functions: GET optener colecciones

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,14 @@
 import * as functions from 'firebase-functions';
 import * as  admin from 'firebase-admin';
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
    credential: admin.credential.cert(serviceAccount),
    databaseURL: "https://game-of-the-year-1ce4f.firebaseio.com"
 })
+
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -15,4 +17,16 @@ export const helloWorld = functions.https.onRequest((request, response) => {
  response.json({
     mensaje: "Hola mundo desde funciones de Firabase!! OK"
  });
+});
+
+ export const getGOTY = functions.https.onRequest( async(request, response) => {
+
+   // const nombre = request.query.nombre;
+
+   const gotyRef  = db.collection('goty');
+   const docsSnap = await gotyRef.get();
+   const juegos = docsSnap.docs.map( doc => doc.data() );
+
+   response.json( juegos );
+
 });
